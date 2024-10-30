7326c45a7d6a2d264f0a1919631d1c0c6ecd9ae9
CNYANGANG <138499348+CNYANGANG@users.noreply.github.com>
2024-10-30 12:11:18 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["yangang.cloud"];
-pub const PUBLIC_RS_PUB_KEY: &str = "5a7ThfSJG3RaxEdo01Y1iFgvtbanbfsboAnhI1cAbuU=";
+pub const PUBLIC_RS_PUB_KEY: &str = "JrYnWk+AnzokXOt87XXwtxgM7xngRLI6hbzTreYAUtU=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
